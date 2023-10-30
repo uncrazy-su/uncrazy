f5907216332323017d776e4f19b7b1b8e9a4a0dd
Raisa <raisa.imani.sani@gmail.com>
2023-10-30 15:16:01 +0700

    Push new change

--- uncrazy/lib/screen/welcome/welcome_screen.dart
+++ uncrazy/lib/screen/welcome/welcome_screen.dart
@@ -6,88 +6,113 @@ import 'package:uncrazy/screen/register/register_screen.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
-              ),
-              Text(
-                'Un-Crazi',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text(
-                    '\"Un-Crazi Your Schedule Now!\"',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    backgroundColor: Colors.blue,
-                    padding: EdgeInsets.only(
-                        left: 100, right: 100, top: 5, bottom: 5),
-                    //side: BorderSide(color: Colors.white, width: 1),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: ((context) => LoginScreen())));
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 20,
+    return Container(
+        constraints: BoxConstraints(
+            minWidth: double.infinity, minHeight: double.infinity),
+        padding: EdgeInsets.only(top: 25.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Un-Crazi",
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
                       color: Colors.white,
-                    ),
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    backgroundColor: Colors.blue,
-                    padding: EdgeInsets.only(
-                        left: 100, right: 100, top: 5, bottom: 5),
-                    //side: BorderSide(color: Colors.white, width: 1),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: ((context) => RegisterScreen())));
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  )),
-              
-              SizedBox(
-                height: 20,
-              ),
-            ],
-          )),
-    );
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
+
+// Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Padding(
+//           padding: EdgeInsets.all(15),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               SizedBox(
+//                 width: MediaQuery.of(context).size.width,
+//                 height: 100,
+//               ),
+//               Text(
+//                 'Un-Crazi',
+//                 style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 35,
+//                     fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(
+//                 height: 100,
+//               ),
+//               Padding(
+//                   padding: EdgeInsets.only(left: 20),
+//                   child: Text(
+//                     '\"Un-Crazi Your Schedule Now!\"',
+//                     style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 25,
+//                         fontWeight: FontWeight.bold),
+//                   )),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               TextButton(
+//                   style: TextButton.styleFrom(
+//                     shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(30))),
+//                     backgroundColor: Colors.blue,
+//                     padding: EdgeInsets.only(
+//                         left: 100, right: 100, top: 5, bottom: 5),
+//                     //side: BorderSide(color: Colors.white, width: 1),
+//                   ),
+//                   onPressed: () {
+//                     Navigator.of(context).push(MaterialPageRoute(
+//                         builder: ((context) => LoginScreen())));
+//                   },
+//                   child: Text(
+//                     'Login',
+//                     style: TextStyle(
+//                       fontSize: 20,
+//                       color: Colors.white,
+//                     ),
+//                   )),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               TextButton(
+//                   style: TextButton.styleFrom(
+//                     shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(30))),
+//                     backgroundColor: Colors.blue,
+//                     padding: EdgeInsets.only(
+//                         left: 100, right: 100, top: 5, bottom: 5),
+//                     //side: BorderSide(color: Colors.white, width: 1),
+//                   ),
+//                   onPressed: () {
+//                     Navigator.of(context).push(MaterialPageRoute(
+//                         builder: ((context) => RegisterScreen())));
+//                   },
+//                   child: Text(
+//                     'Register',
+//                     style: TextStyle(
+//                       fontSize: 20,
+//                       color: Colors.white,
+//                     ),
+//                   )),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//             ],
+//           )),
+//     )
